c9ce2bc0eac7707bac2bd16964f2e8a9555ef3a0
shams <shams.orakzai.777@gmail.com>
2022-03-29 20:01:14 +0500

    Adding two numbers program implemented.

---
+++ Easy_problems/1. Add_two_numbers.py
@@ -0,0 +1,15 @@
+number_1 = int(input("Enter number: 1 -> "))
+number_2 = int(input("Enter number: 2 -> "))
+
+result = number_1 + number_2
+result_in_float = float(number_1) + float(number_2)
+
+print(result)
+print("Sum of {0} and {1} is {2}".format(number_1, number_2, result))
+print(f"Sum of {number_1} and {number_2} is {result}")
+print(f"Sum of {number_1} and {number_2} in Float is {result_in_float}")
+
+
+
+
+
